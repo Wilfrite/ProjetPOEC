69ea4747422ab75cb7cfc24b3e39d424085bce68
Kevin <duchennekevin@hotmail.fr>
2014-09-11 10:43:16 +0200

    maj controller

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -13,6 +13,23 @@ abstract class Controller {
             return $this->config['href']."index.php?c=$controller&a=$action&p=$params";
         }
     }
+    /**
+     * @param string $type
+     * @param $message
+     */
+    protected  function setFlash($message, $type='warning') {
+        $_SESSION['flash']['message'] = $message;
+        $_SESSION['flash']['type'] = $type;
+    }
+    protected function flash() {
+        if (isset($_SESSION['flash']))
+        {
+            $flash =  $_SESSION['flash'];
+            unset($_SESSION['flash']);
+            return $flash;
+        }
+        return null;
 
+    }
 
 } 
