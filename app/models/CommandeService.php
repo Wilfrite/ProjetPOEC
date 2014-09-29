099f0b1590c3c9b6bcbb542ba2d0b78a1ed62328
Kevin <duchennekevin@hotmail.fr>
2014-09-29 16:15:02 +0200

    commit of  doom, commandeservice a tester sous grande modération.

--- app/models/CommandeService.php
+++ app/models/CommandeService.php
@@ -11,13 +11,36 @@ class CommandeService {
     function __construct(PDO $dbh) {
         $this->dbh = $dbh;
     }
-    public function createOrder( $adresse_livraison,$cp_livraison,$cp_livraison,$ville_livraison,$adresse_facturation,$cp_facturation,$ville_facturation,$id_user)
+
+    public  function  createLinkOrder($id_commande, $array)
+    {
+        try {
+            $valuesSql = null;
+
+            $sql = "INSERT INTO `article_commande`(`quantite`, `id`, `id_commande`) VALUES (:quantite,:id,:id_commande)";
+            $stmt = $this->dbh->prepare($sql);
+            foreach ($array as $link)
+            {
+
+                $stmt->execute([
+                        ':quantite' => $link,
+                        ':id' =>  key($link),
+                        ':id_commande' =>$id_commande
+                        
+                    ]);
+            }
+        } catch (PDOException $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    public function createOrder( $adresse_livraison ,$cp_livraison , $cp_livraison , $ville_livraison , $adresse_facturation , $cp_facturation ,$ville_facturation , $id_user, $array)
     {
         try {
             $sql = "INSERT INTO `commande`( `statut`, `date_commande`, `date_reception`, `adrersse_livraison`, `cp_livraison`, `ville_livraison`, `adrersse_facturation`, `cp_facturation`, `ville_facturation`, `id_utilisateur`) VALUES ( 'en préparation', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP +2, :adrersse_livraison, :cp_livraison, :ville_livraison, :adrersse_facturation, :cp_facturation, :ville_facturation, :id_utilisateur)";
             $stmt = $this->dbh->prepare($sql);
             $stmt->execute([
-                    ':statut' => 'en préparation',
+
                     ':adrersse_livraison' => $adresse_livraison,
                     ':cp_livraison' =>$cp_livraison,
                     ':ville_livraison' =>$ville_livraison,
@@ -26,10 +49,12 @@ class CommandeService {
                     ':ville_facturation' =>$ville_facturation,
                     ':id_utilisateur' =>$id_user,
                 ]);
-            $stmt = NULL;
+           $result = $stmt->lastInsertId();
         } catch (PDOException $e) {
             die('Erreur : ' . $e->getMessage());
         }
+        $this->createLinkOrder($result,$array);
         return (isset($result) ? $result : null);
+
     }
 }
